6cb1d580e98742a6aecb07176b000b89a91fea4c
praveenkumar.N <praveenkumar118211@gmail.com>
2018-08-06 16:18:51 +0530

    Update index.py

--- hunter/index.py
+++ hunter/index.py
@@ -1,6 +1,6 @@
 def sorted(arr,start,end)
 if(start>end):
-return
+return 
 print sorted(arr,start*2+1,end)
 print(arr[start])
 print sorted(arr,start*2+2,end)
@@ -8,4 +8,3 @@ def main():
 arr[]=[1,5,3,4,2,6]
 print(arr[i])
 print(arr,0,arr_size-1)
-return
