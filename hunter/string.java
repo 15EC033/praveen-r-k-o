bfa51c484fa4292baea8a6ef42e5a9031367b1ae
praveenkumar.N <praveenkumar118211@gmail.com>
2018-08-30 14:51:49 +0530

    Create string.java

---
+++ hunter/string.java
@@ -0,0 +1,9 @@
+public class StringReverse 
+{
+   public static void main(String[] args)
+   {
+	   String s1="RANDY";
+	   String s2=new StringBuffer(s1).reverse().toString();
+	   System.out.println(s2);
+   }
+}
